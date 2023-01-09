c778f8555893c5a491be96dd1c686e810eb2257d
Bluebender <olliviev@gmail.com>
2023-01-09 11:47:51 +0000

    Sauvegarde de 13h

---
+++ Mini Projet Interface d'Administration/assets/js/index.js
@@ -0,0 +1,30 @@
+let btn_utilisateur = document.getElementById("btn_utilisateur");
+let menu_utilisateur = document.getElementById("menu_utilisateur");
+let btn_produits = document.getElementById("btn_produits");
+let menu_produits = document.getElementById("menu_produits");
+let btn_categorie = document.getElementById("btn_categorie");
+let menu_categorie = document.getElementById("menu_categorie");
+
+let btn_nav = document.getElementById("btn_nav");
+let menu_nav = document.getElementById("menu_nav");
+
+let section_header = document.getElementById("section_header");
+
+function afficherLesSousMenus (bouton, menu){
+    bouton.addEventListener("click", function(event){
+       menu.classList.toggle("hidden"); 
+    });
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+
+// Ouverture des sous-menus
+afficherLesSousMenus(btn_utilisateur, menu_utilisateur);
+afficherLesSousMenus(btn_produits, menu_produits);
+afficherLesSousMenus(btn_categorie, menu_categorie);
+
+// Ouverture du menu principale
+afficherLesSousMenus(btn_nav, menu_nav);
+afficherLesSousMenus(btn_nav, section_header);
+
+});
